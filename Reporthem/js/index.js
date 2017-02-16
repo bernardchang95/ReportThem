6ff078709318c8e9e4a0a93cebb5ed5a25174b75
Bernard Chang <bernchang@outlook.com>
2017-02-16 22:36:09 +0800

    Updates uploaded

---
+++ Reporthem/js/index.js
@@ -0,0 +1,13 @@
+﻿function createNew() {
+    var newEmailLink = document.createElement('newEmailLink');
+    var newEmailText = document.createTextNode("newEmailText");
+    newEmailLink.appendChild(newEmailText);
+    newEmailLink.title(newEmailText);
+    newEmailLink.href('Forums.html');
+    document.body.appendChild(newEmailLink);
+    document.write(newEmailLink);
+}
+
+function storeData() {
+    createNew();
+}
