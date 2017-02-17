a3d0ca5500ac56d181c37568f372d9c37ac2b67a
Bernard Chang <bernchang@outlook.com>
2017-02-17 15:55:33 +0800

    Buttons changed to input tags. Added .js and .css

--- Reporthem/js/index.js
+++ Reporthem/js/index.js
@@ -1,13 +1,1 @@
-﻿function createNew() {
-    var newEmailLink = document.createElement('newEmailLink');
-    var newEmailText = document.createTextNode("newEmailText");
-    newEmailLink.appendChild(newEmailText);
-    newEmailLink.title(newEmailText);
-    newEmailLink.href('Forums.html');
-    document.body.appendChild(newEmailLink);
-    document.write(newEmailLink);
-}
-
-function storeData() {
-    createNew();
-}
+﻿
